8db9945ebd603b323eae856978226e766a1eced8
Austen Collins <austen@serverless.com>
2019-07-05 22:20:41 -0700

    version bump

--- src/Component.js
+++ src/Component.js
@@ -80,7 +80,7 @@ class Component {
     this.context.log = (msg) => this.context.instance.log(msg)
     this.context.debug = (msg) => this.context.instance.debug(msg)
     this.context.status = (msg, entity) => this.context.instance.status(msg, entity || this.id)
-    this.context.output = (key,val) => this.context.instance.output(key, val)
+    this.context.output = (key, val) => this.context.instance.output(key, val)
   }
 
   async save() {
